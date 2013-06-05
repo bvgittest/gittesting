c3f7a24b4db1b799c96f5ac0085b9a14118fc259
Edward <eklapatiuk@gmail.com>
2013-06-05 15:17:51 -0400

    Added message to button 1

--- gittesting/Form1.cs
+++ gittesting/Form1.cs
@@ -16,5 +16,10 @@ namespace gittesting
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Message from button 1");
+        }
     }
 }
