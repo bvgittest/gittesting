6c9c685046bc0be36e52ac33e86d1197ff6513bf
kewal <kewal.sehgal@gmail.com>
2013-06-05 15:17:52 -0400

    I added code for 2nd button

--- gittesting/Form1.cs
+++ gittesting/Form1.cs
@@ -16,5 +16,10 @@ namespace gittesting
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("2ndButton");
+        }
     }
 }
